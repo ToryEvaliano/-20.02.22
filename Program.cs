c42c5f7ba7a5e6e08f789d0e8bd14cc990ef91d4
EA <trestluk@yandex.ru>
2022-02-20 14:25:05 +0400

    Create array of string and print it out

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿// // Программа, кот-ая из массива строк форм-т массив из строк, длина кот-х <= 3 символам
+
+string [] arrayString = new string [5] {"Hello", "Hi", "Privet", "Chao", "Ola"};
+for (int count = 0; count < 5; count++)
+{
+    Console.Write(arrayString[count]);
+    Console.Write(" ");
+}
+Console.WriteLine(" ");
